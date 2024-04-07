c1f05ce54f1c3d82c3617ba5ad7faf834453f01b
Tiago Costa <89879117+tiagomaltacosta@users.noreply.github.com>
2024-04-06 21:49:40 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,52 @@
 db = {};
 
 db.init = function(callback) {
-    $.getJSON('data/db.json', function(data) {
+    $.getJSON('db.json', function(data) {
         db.data = data;
         if(callback) {
             callback();
         }
     });
 }
+function initSelect() {
+    var select = $('#slc-topicos');
+    $.each(db.data, function(index, item) {
+        select.append($('<option>', {
+            value: item.id,
+            text: item.titulo
+        }));
+    });
+}
+$(document).ready(function() {
+    // Carregar o JSON e preencher o combobox
+    $.getJSON('db.json', function(data) {
+        $.each(data, function(index, item) {
+            $('#slc-topicos').append($('<option>', {
+                value: item.id,
+                text: item.titulo
+            }));
+        });
+    });
+
+    // Evento de mudança no combobox
+    $('#slc-topicos').change(function() {
+        var selectedId = $(this).val();
+        if (selectedId !== '') {
+            // Buscar dados no JSON com base no ID selecionado
+            $.getJSON('db.json', function(data) {
+                var selectedItem = data.find(function(item) {
+                    return item.id == selectedId;
+                });
+                if (selectedItem) {
+                    // Exibir descrição no campo de texto
+                    $('#displayText').val(selectedItem.descricao);
+                }
+            });
+        } else {
+            // Limpar campo de texto se nenhuma opção for selecionada
+            $('#displayText').val('');
+        }
+    });
+});
+
+    db.init(initSelect);
